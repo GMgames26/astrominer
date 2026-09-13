6d0a8c6435baf8d3700eb32d0694465ed79f906a
GMgames26 <giovannimurdaca7@gmail.com>
2026-09-13 23:40:35 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// Astrominer - service worker minimo: rende l'app installabile e giocabile offline.
+var CACHE_NAME = "astrominer-cache-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k!==CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      var networkFetch = fetch(event.request).then(function(response){
+        if(response && response.ok){
+          var copy = response.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+        }
+        return response;
+      }).catch(function(){ return cached; });
+      return cached || networkFetch;
+    })
+  );
+});
