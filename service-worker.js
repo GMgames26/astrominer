b307cf0044017a33d16b076604c57a76f3fc0cbc
GMgames26 <giovannimurdaca7@gmail.com>
2026-09-14 14:09:18 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Astrominer - service worker minimo: rende l'app installabile e giocabile offline.
-var CACHE_NAME = "astrominer-cache-v13";
+var CACHE_NAME = "astrominer-cache-v14";
 var ASSETS = [
   "./",
   "./index.html",
